25a75c5c6f623974d81973dba253d174dcbb0605
xred-spec <luiscarranza1216@gmail.com>
2026-05-21 20:31:28 -0600

    apiResource de usuarios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Rutas Usuario
+Route::apiResource('usuarios', UsuarioController::class);
